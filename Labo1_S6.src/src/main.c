92e859430f60466d6da1d79985e849fcd69a2b27
RUSTENHOLZ Hugo <h0rusten@enib.fr>
2023-02-05 12:46:08 +0100

    finish labo1

--- Labo1_S6.src/src/main.c
+++ Labo1_S6.src/src/main.c
@@ -7,7 +7,7 @@
 
 
 
-#define MAIN2
+#define MAIN3
 
 #ifdef MAIN1
 
@@ -122,33 +122,91 @@ int main()
 #define sampling_period 100         //us
 #define sw_up_delay_base 5000       //delay = n * sampling_period
 #define sw_down_delay_base 5000     //delay = n * sampling_period
+
+
+#define SW_UP_MASK      (1<<4)
+#define SW_DOWN_MASK    (1<<0)
     
 /* main test program */
 int main()
 {
-	uint32_t dutycycle = 0;
+	uint32_t dutycycle = 50;
     uint32_t sw_up_delay = 0;
     uint32_t sw_down_delay = 0;
 	
     lcd_reset();
     // Configure switches
     sw_init();
+
+    cls();
+    lcd_printf("dutycycle: %d", dutycycle);
     
     // Configure output LED_RED:
-//	io_configure(    A COMPLETER    );
+    //	io_configure(    A COMPLETER    );
+    io_configure(_GPIOB, PIN_4, ( PIN_MODE_ALTFUNC | PIN_OPT_AF2 | PIN_OPT_RESISTOR_NONE | PIN_OPT_OUTPUT_PUSHPULL ), NULL);
+
 	// Configure output LED_GREEN:
-//	io_configure(    A COMPLETER    );
+    io_configure(_GPIOC, PIN_7, ( PIN_MODE_ALTFUNC | PIN_OPT_AF2 | PIN_OPT_RESISTOR_NONE | PIN_OPT_OUTPUT_PUSHPULL ), NULL);
+    //	io_configure(    A COMPLETER    );
     
 	
     //PWM configurations
-//      A COMPLETER        
+    //      A COMPLETER            
+    pwm_init(_TIM3, pwm_period, NULL);
+    
+    //pwm_channel_enable(    A COMPLETER    ); 
+    pwm_channel_enable(_TIM3, PWM_CHANNEL_1, dutycycle, 1); //red timer
+    pwm_channel_enable(_TIM3, PWM_CHANNEL_2, 100-dutycycle, 1); //green timer
+
+    pwm_start(_TIM3);
+
+    void changeDutyCycle(uint32_t delta) {
+        dutycycle += delta;
+
+        if (dutycycle<0) {
+            dutycycle = 0;
+        } else if (dutycycle>100) {
+            dutycycle = 100;
+        }
+
+        pwm_channel_set(_TIM3, PWM_CHANNEL_1, dutycycle);
+        pwm_channel_set(_TIM3, PWM_CHANNEL_2, 100-dutycycle);
+        cls();
+        lcd_printf("dutycycle: %d %d", dutycycle, 100-dutycycle);
+    }
+
 	
 	while (1) {
-//      timer_wait_us(    A COMPLETER    );
-        
-        //          < CODE PRINCIPAL A COMPLETER >
+        uint32_t left = sw_left();
+        uint32_t right = sw_right();
+        if (left && dutycycle>0) {
+            changeDutyCycle(-10);
+        } else if (right && dutycycle<100) {
+            changeDutyCycle(10);
+        }
 
         
+        if (io_read(_GPIOA, SW_UP_MASK)) {
+            ++sw_up_delay;
+            if (sw_up_delay>=sw_up_delay_base) {
+                changeDutyCycle(10);
+                sw_up_delay = 0;
+            }
+        } else {
+            sw_up_delay = 0;
+        }
+
+        if (io_read(_GPIOB, SW_DOWN_MASK)) {
+            ++sw_down_delay;
+            if (sw_down_delay>=sw_down_delay_base) {
+                changeDutyCycle(-10);
+                sw_down_delay = 0;
+            }
+        } else {
+            sw_down_delay = 0;
+        }
+
+        timer_wait_us(_TIM2, sampling_period, NULL); 
 	}
 	
 	return 0;
